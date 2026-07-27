c65db23683350ad07a8fcefff8d74cfb7399610b
tonysoprano33 <lautarovirtual209@gmail.com>
2026-07-27 06:46:45 -0300

    Remove bronchodilator reminder from agenda

--- src/clinic/tests.py
+++ src/clinic/tests.py
@@ -895,7 +895,7 @@ class DashboardInlineUpdateTests(TestCase):
         self.assertIn('title="Guardar SO2 y FC en reposo"', html)
         self.assertIn('title="Guardar SO2 y FC post"', html)
 
-    def test_espirometrista_dashboard_renders_discreet_operational_tools(self):
+    def test_espirometrista_dashboard_hides_bronchodilator_control_but_keeps_operational_tracking(self):
         session = self.client.session
         session["clinic_work_mode"] = "espirometrista"
         session.save()
@@ -905,8 +905,8 @@ class DashboardInlineUpdateTests(TestCase):
         self.assertEqual(response.status_code, 200)
         html = response.content.decode()
         self.assertIn("data-waiting-elapsed", html)
-        self.assertIn("data-bronchodilator-reminder", html)
-        self.assertIn("start_bronchodilator_timer", html)
+        self.assertNotIn('<td class="bronchodilator-cell"', html)
+        self.assertNotIn('<th class="col-vitals">Broncodilatador</th>', html)
 
     def test_espirometrista_dashboard_uses_review_bands_instead_of_a_duplicate_status(self):
         SpirometryResult.objects.create(encounter=self.encounter, respiratory_pattern="Normal")
